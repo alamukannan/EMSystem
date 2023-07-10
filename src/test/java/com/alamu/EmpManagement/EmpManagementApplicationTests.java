b41d0d23fc6b5a62f51f94dc6fb8ee246c6abaf2
alamukannan <alamskanna@gmail.com>
2023-07-10 19:46:14 +0200

    my initial project setup

---
+++ src/test/java/com/alamu/EmpManagement/EmpManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamu.EmpManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
